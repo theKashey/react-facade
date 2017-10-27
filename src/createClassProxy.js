72af8174387691b8f799fbc1c59b310d099897d6
Anton Korzunov <akorzunov@atlassian.com>
2017-10-27 22:28:38 +1100

    cover arrow functions

--- src/createClassProxy.js
+++ src/createClassProxy.js
@@ -2,6 +2,9 @@ import {Component} from 'react';
 import find from 'lodash/find';
 import transferStaticProps from './staticProps';
 
+const GENERATION = '__facade__proxyGeneration';
+const REGENERATION = '__facade__regenerateByEval'
+
 function getDisplayName(Component) {
   const displayName = Component.displayName || Component.name;
   return (displayName && displayName !== 'ReactComponent')
@@ -32,20 +35,28 @@ const lifeCycleMethods = [
 ];
 
 function checkClassMembers(ProxyComponent, NextComponent) {
+  const injectedCode = {};
   try {
     const ins1 = new ProxyComponent({}, {});
     const ins2 = new NextComponent({}, {});
     const mergeProps = Object.assign({}, ins1, ins2);
     Object
       .keys(mergeProps)
+      .filter(key => !key.startsWith('__facade__'))
       .forEach(function (key) {
         if (("" + ins1[key]) != ("" + ins2[key])) {
-          console.error('React-facade:', ' updated class ', ProxyComponent.name, 'had different code for', key, ins2[key], '. Unable to reproduce');
+
+          if(!ins1[REGENERATION]){
+            console.error('React-facade:', ' Updated class ', ProxyComponent.name, 'had different code for', key, ins2[key], '. Unable to reproduce');
+          } else {
+            injectedCode[key] = ins2[key];
+          }
         }
       });
   } catch (e) {
 
   }
+  return injectedCode;
 }
 
 function checkLifeCycleMethods(ProxyComponent, NextComponent) {
@@ -55,14 +66,13 @@ function checkLifeCycleMethods(ProxyComponent, NextComponent) {
     lifeCycleMethods
       .forEach(function (key) {
         if (("" + p1[key]) != ("" + p2[key])) {
-          console.error('React-facade:', ' updated class ', ProxyComponent.name, 'had different code for lifecycle method', key, p2[key], '. Unable to repeat');
+          console.error('React-facade:', 'You did update', ProxyComponent.name, '\s lifecycle method', key, p2[key], '. Unable to repeat');
         }
       });
   } catch (e) {
 
   }
 }
-
 
 function proxyClass(InitialComponent) {
   // Prevent double wrapping.
@@ -75,6 +85,18 @@ function proxyClass(InitialComponent) {
   let CurrentComponent;
   let ProxyComponent;
   let savedDescriptors = {};
+  let injectedMembers = {};
+  let proxyGeneration = 0;
+
+  let inject = (target) => {
+    if(target[GENERATION]!=proxyGeneration){
+      Object
+        .keys(injectedMembers)
+        .forEach(key => target[REGENERATION](key, injectedMembers[key]));
+
+      target[GENERATION]=proxyGeneration;
+    }
+  }
 
   let StatelessProxyComponent = class StatelessProxyComponent extends Component {
     render() {
@@ -87,7 +109,12 @@ function proxyClass(InitialComponent) {
     : StatelessProxyComponent;
 
   ProxyComponent = class ProxiedComponent extends InitialParent {
+    constructor(props, context) {
+      super(props, context);
+      this[GENERATION] = proxyGeneration;
+    }
     render() {
+      inject(this);
       return isClass(CurrentComponent)
         ? CurrentComponent.prototype.render.call(this)
         : CurrentComponent(this.props, this.context);
@@ -112,6 +139,8 @@ function proxyClass(InitialComponent) {
       return update(existingProxy.__getCurrent());
     }
 
+    proxyGeneration++;
+
     // Save the next constructor so we call it
     const PreviousComponent = CurrentComponent;
     CurrentComponent = NextComponent;
@@ -133,9 +162,10 @@ function proxyClass(InitialComponent) {
     if (isClass(NextComponent)) {
       checkLifeCycleMethods(ProxyComponent, NextComponent);
       Object.setPrototypeOf(ProxyComponent.prototype, NextComponent.prototype);
-      checkClassMembers(ProxyComponent, NextComponent);
+      injectedMembers = checkClassMembers(ProxyComponent, NextComponent);
     } else {
       ProxyComponent.prototype.prototype = StatelessProxyComponent.prototype;
+      injectedMembers = {};
     }
   };
 
